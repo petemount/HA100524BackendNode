b02b3c2f85978a78229a8934d9d59bfb6defe682
Github_Name <petemount@t-online.de>
2024-05-10 15:26:34 +0200

    HA100524BACKENDNODE

---
+++ externalModule.js
@@ -0,0 +1,7 @@
+const random = require('random-string-generator');
+
+console.log(random());  // 'qCCm2Yoyycjm' or others
+console.log(random('numeric'));  
+console.log(random('uppernumeric'));  
+console.log(random('lowernumeric'));  
+console.log(random('lower'));  
